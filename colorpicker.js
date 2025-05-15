ccc8f90481f62c4085b101554d14cda0ebcc967e
beattelljuice <38844499+beattelljuice@users.noreply.github.com>
2025-05-15 15:06:26 +1000

    Pushed Temperature Functionality

--- colorpicker.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const colorInput = document.getElementById("colorInput");
-  const colorValue = document.getElementById("colorValue");
-
-  colorInput.addEventListener("input", () => {
-    colorValue.textContent = colorInput.value;
-  });
-});
